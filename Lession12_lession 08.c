b336f4b2daa92a45c24790e518bbc3ac5958797d
hoang180206 <hoangnguyen29011802@gmail.com>
2024-12-10 23:12:05 +0700

    Add files via upload

---
+++ Lession12_lession 08.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int UCLN(int a, int b){
+    while (b != 0){
+        int temp=b;
+        b=a%b;
+        a=temp;
+    }
+    return a;
+}
+
+int main(){
+    int num1, num2;
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &num1);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &num2);
+
+    int result = UCLN(num1, num2);
+    printf("Uoc chung lon nhat cua %d va %d la: %d\n", num1, num2, result);
+
+    return 0;
+}
